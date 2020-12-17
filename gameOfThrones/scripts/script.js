9e4f2f1fa6ee31b96c360ef14831b474c1015f8a
Ally Deeter <allydeeter6639@gmail.com>
2020-12-17 08:15:52 -0800

    adds Game of Thrones API exercise

--- gameOfThrones/scripts/script.js
+++ gameOfThrones/scripts/script.js
@@ -25,33 +25,108 @@ $(() => {
     
     .then(data =>{
     
-            let objectArr = [];
-    
-            data.forEach((char) => {
-                objectArr = [...objectArr, ...char]
-    
-        })
-        console.log(objectArr)
+        let objectArr = [];
 
-        objectArr.forEach((object) => {
+        data.forEach((char) => {
+            objectArr = [...objectArr, ...char]
 
-            if (object.name != ""){
-                let name = object.name;
-                let listItemName = $('<li>').html(`Name: ${name}`);
-                $('ul').append(listItemName);
-                if (object.allegiances != ""){
-                    let house = object.allegiances;
-                    let listItemHouse = $('<li>').html(`House: ${house}`);
-                    $('ul').append(listItemHouse);
-                }
-            } else{
-                let name = object.aliases[0];
-                let listItem = $('<li>').html(`Character name: ${name}`);
-                $('ul').append(listItem);
-            }
-                
-        })
     })
+
+
+        let $listGroup = $('.list-group');
+        let liTags = objectArr.map((object) => {
+            if (object.name){
+                return `<a href="${object.url}" class="list-group-item list-group-item-action"><b>${object.name} </b>| Houses: ${object.allegiances.length} </a>`
+            } else {
+                return `<a href="${object.url}" class="list-group-item list-group-item-action"><b>${object.aliases[0]} </b>| Houses: ${object.allegiances.length} </a>`
+            }
+            
+        })
+
+        $listGroup.html(liTags.join(''))
+                
+    })
+
+    let $div = $('.list-group');
+
+    $div.click((e) => {
+
+        console.log("i'm here");
+        e.preventDefault();
+
+        $.get(e.target.href)
+        .done((moreInfoChar) => {
+            
+            // console.log(object.name);
+
+            let $modalBody = $(`.modal-body`);
+            let $modalTitle = $(`#exampleModalLabel`);
+            let $modalDialog = $(`#exampleModal`);
+            let name = moreInfoChar.name;
+            // let alias = moreInfoChar.aliases[0];
+            let houses = moreInfoChar.allegiances;
+
+           
+
+            $modalTitle.html("");
+            $modalBody.html("");
+
+            if (name){
+                $modalTitle.html(name);
+            } else{
+                if (moreInfoChar.aliases){
+                    $modalTitle.html(moreInfoChar.aliases[0]);
+                }
+                
+            }
+
+            if (houses){
+
+                houses.forEach((houseURL) => {
+                    $.get(houseURL)
+                    .done((houseObj)=>{
+                        $modalBody.html(`<br>${$modalBody.html()}<br>${houseObj.name}`)
+                    })
+                })
+            }
+
+            $modalDialog.modal('show');
+
+        })
+
+
+    })
+
+
+            
+            
+                
+        // })
+        
+        // objectArr.forEach(async (object) => {
+        //     if (object.allegiances && object.allegiances.length > 0) {
+                
+        //         let response = await fetch(object.allegiances[0]).then(res => res.json());
+                
+                // if (object.name != ""){
+                //     let name = object.name;
+                //     let listItemName = $('<li>').html(`Name: ${name}`);
+                //     $('ul').append(listItemName);
+                    
+        //             const anchor = $(`<a href=${response.url}>`).html(`House: ${response.name}`)
+        //             let listItemHouse = $('<li>').append(anchor);
+        //             $('ul').append(listItemHouse);                  
+        //         } else{
+        //             let name = object.aliases[0];
+                    
+        //             let listItem = $('<li>').html(`Name: ${name}`);
+        //             $('ul').append(listItem);
+        //         }
+                
+        //     }          
+                
+        // })
+    // })
     
 })
 
